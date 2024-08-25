098ebb2297dc51c2c308e6d583905b01ef2de119
axelker <axelker@outlook.fr>
2024-08-25 20:16:12 +0200

    feat: Load do env in the app config

--- back/config/config.py
+++ back/config/config.py
@@ -1,10 +1,12 @@
 import os
-
+from dotenv import load_dotenv
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')\
         or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    ALLOWED_ORIGINS = os.environ.get('ALLOWED_ORIGINS', '').split(',')
